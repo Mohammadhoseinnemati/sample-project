ffc3cc7c86951065e4f69ac14d944124773dc808
mh.nemati <123456@Aa>
2024-02-28 18:25:12 +0330

    onchange

--- javascript/page2/product.js
+++ javascript/page2/product.js
@@ -1,4 +1,4 @@
-const data = [
+const products = [
     {
         name: "محصول 1",
         description: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه",
@@ -76,18 +76,60 @@ const data = [
     }
 ];
 
-data.forEach((product, i) => {
-    const productBox = $("#products");
-    const template = `
-    <div class="col-lg-4 col-md-6">
-        <div class="card my-2">
-            <img src="${product.image}" class="card-img-top img-fluid" alt="تضویر محصول شماره : ${i + 1}">
-            <div class="card-body">
-                <h5 class="card-title">${product.name}</h5>
-                <p class="card-text">${product.description}</p>
-            </div>
-        </div>
-    </div>
-    `;
-    $(productBox).append(template);
+
+const renderData = (data) => {
+    data.forEach((product, i) => {
+        const productBox = $("#products");
+        const template = `
+                <div class="col-lg-4 col-md-6 py-2">
+                    <div class="card h-100">
+                        <img src="${product.image}" class="card-img-top img-fluid" alt="تضویر محصول شماره : ${i + 1}">
+                        <div class="card-body">
+                            <h5 class="card-title">${product.name}</h5>
+                            <p class="card-text">${product.description}</p>
+                            <div class="mt-2">
+                                <ul class="react-box d-flex justify-content-end mb-0">
+                                    <li class="me-3 order-2" onclick="like(this)">
+                                        <i class="fa-regular fa-heart"></i>
+                                    </li>
+                                    <li class="me-3 order-1">
+                                        <i class="fa-solid fa-paper-plane"></i>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        `;
+        $(productBox).append(template);
+    });
+}
+
+
+$(document).ready(() => {
+    renderData(products);
 });
+
+const like = (element) => {
+    const isLiked = $(element).hasClass("liked");
+    if (!isLiked) {
+        $(element).addClass("liked");
+    } else {
+        $(element).removeClass("liked");
+    }
+}
+
+const renderFilteredData = (filter) => {
+    const filteredData = products.filter((product) => product.name.includes(filter) && product);
+    const productBox = $("#products");
+    $(productBox).html(null);
+    renderData(filteredData);
+}
+
+var timeOut;
+const changeFilterInput = (element) => {
+    if (timeOut) {
+        clearTimeout(timeOut);
+    }
+    timeOut = setTimeout(() => renderFilteredData(element.value), 3000)
+}
