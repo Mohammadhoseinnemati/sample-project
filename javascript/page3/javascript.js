40060bfa2c1ae9060eafc5dc3bf8cd991a0e1629
Mohammad <developerworld1402@gmail.com>
2024-02-29 17:13:53 +0330

    page 3 responsive fix

--- javascript/page3/javascript.js
+++ javascript/page3/javascript.js
@@ -2,12 +2,34 @@ $(document).ready(() => {
     $('#galleryCarousel').slick({
         rtl: true,
         prevArrow: "<button type=\"button\" class=\"slick-custom-arrow custom-prev-arrow text-black\"><i class=\"fa-solid fa-arrow-right\"></i></button",
-        nextArrow: "<button type=\"button\" class=\"slick-custom-arrow custom-next-arrow text-black\"><i class=\"fa-solid fa-arrow-left\"></i></button"
+        nextArrow: "<button type=\"button\" class=\"slick-custom-arrow custom-next-arrow text-black\"><i class=\"fa-solid fa-arrow-left\"></i></button",
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    arrows: false
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
     $('#productAttributes').slick({
         rtl: true,
         prevArrow: "<button type=\"button\" class=\"slick-custom-arrow custom-prev-arrow text-black\"><i class=\"fa-solid fa-arrow-right\"></i></button",
-        nextArrow: "<button type=\"button\" class=\"slick-custom-arrow custom-next-arrow text-black\"><i class=\"fa-solid fa-arrow-left\"></i></button"
+        nextArrow: "<button type=\"button\" class=\"slick-custom-arrow custom-next-arrow text-black\"><i class=\"fa-solid fa-arrow-left\"></i></button",
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    arrows: false
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 });
 
